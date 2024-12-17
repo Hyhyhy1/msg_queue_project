3393613cfa7ece37b907ae9eb769180069cebf02
Hyhyhy1 <nikita-kuleshov-2003@mail.ru>
2024-12-17 20:55:21 +0500

    Sender api v1

---
+++ gateway_app/gateway.py
@@ -0,0 +1,24 @@
+import uvicorn
+from fastapi import FastAPI
+from contextlib import asynccontextmanager
+from notification_router import router
+
+#@asynccontextmanager
+#async def lifespan(app: FastAPI):
+#    database_ = app.state.database
+#    if not database_.is_connected:
+#        await database_.connect()
+#
+#    yield
+#
+#    database_ = app.state.database
+#    if database_.is_connected:
+#        await database_.disconnect()
+
+#app = FastAPI(lifespan=lifespan)
+app = FastAPI()
+
+app.include_router(router, prefix="/api")
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
